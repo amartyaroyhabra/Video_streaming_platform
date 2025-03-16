300c45b74d0c596b8bd8037f827f142727e292a2
AMARTYA ROY <60499684+amartyaroyhabra@users.noreply.github.com>
2025-03-16 23:51:13 +0530

    Add files via upload

---
+++ springboot-video-stream-backend/src/main/java/com/videoStream/app/SpringbootVideoStreamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.videoStream.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVideoStreamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVideoStreamBackendApplication.class, args);
+	}
+
+}
---
+++ springboot-video-stream-backend/src/test/java/com/videoStream/app/SpringbootVideoStreamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoStream.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVideoStreamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
